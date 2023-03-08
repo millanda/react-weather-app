16884fb786dab1b75f9e91e9e54bc40e9c7f5e0f
millanda <97857799+millanda@users.noreply.github.com>
2023-03-08 14:04:23 -0800

    Added footer at bottom with link to Github Repository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Millanda Zhou and is{" "}
+        <a href="https://github.com/millanda/react-weather-app">
+          open-sourced on Github
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
