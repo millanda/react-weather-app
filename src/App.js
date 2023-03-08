c21d8df301d4c005c9044266edb4c90caf1fcf15
millanda <97857799+millanda@users.noreply.github.com>
2023-03-08 14:10:32 -0800

    Imported bootstrap and added container

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Millanda Zhou and is{" "}
-        <a href="https://github.com/millanda/react-weather-app">
-          open-sourced on Github
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Millanda Zhou and is{" "}
+          <a href="https://github.com/millanda/react-weather-app">
+            open-sourced on Github
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
