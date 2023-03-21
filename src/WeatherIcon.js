be3cc13e2df4c1dfffed4da7271d4a88e5a1c93e
millanda <97857799+millanda@users.noreply.github.com>
2023-03-21 16:27:36 -0700

    Imported open-source icon font of Weather icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./weather-icons.css";
+import "./css/weather-icons.css";
 
 export default function WeatherIcon(props) {
   return <div className="WeatherIcon"> Hi <i className="wi wi-day-sunny"></i></div>;
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -11,7 +11,7 @@ export default function WeatherInfo(props) {
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">Mostly Cloudy</li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>{" "}
       <div className="row mt-3">
         <div className="col-6 d-flex justify-content-center">
