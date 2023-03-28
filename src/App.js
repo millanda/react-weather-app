48da7c5876ee56e0f9f89d13c314cc90e846e2de
millanda <97857799+millanda@users.noreply.github.com>
2023-03-28 00:22:24 -0700

    Updated styling for background-color and font-colors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="container weather-app-wrapper">
         <Weather defaultCity="Seattle" />
         <footer>
           This project was coded by Millanda Zhou and is{" "}
